eecb013ec4d5ca88879cb1a0252e1664340fe6bd
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-08-07 15:41:31 +0300

    added exceptions for orders

--- app/orders/utils.py
+++ app/orders/utils.py
@@ -1,7 +1,8 @@
 from datetime import time
 
-from fastapi import Query, HTTPException
+from fastapi import Query
 from app.orders.config_orders import START_WALK_TIME, END_WALK_TIME
+from app.orders.exceptions import UncorrectedTimeOrderHTTPException, TimeIntervalOrderHTTPException
 
 
 def validate_walk_time(walk_time: time = Query(...,
@@ -11,8 +12,7 @@ def validate_walk_time(walk_time: time = Query(...,
                                                example="07:00"
                                                )) -> time:
     if walk_time.minute not in [0, 30]:
-        raise HTTPException(status_code=400, detail=f"Время прогулки должно приходиться на начало часа или получаса")
+        raise UncorrectedTimeOrderHTTPException
     if not (START_WALK_TIME <= walk_time <= END_WALK_TIME):
-        raise HTTPException(status_code=400,
-                            detail=f"Прогулки могут быть запланированы только с {START_WALK_TIME} до {END_WALK_TIME}")
+        raise TimeIntervalOrderHTTPException
     return walk_time
