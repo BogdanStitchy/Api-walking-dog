41959dc3f8c5c2e1be677580e09ccdd34bd56ac9
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-08-07 18:48:45 +0300

    added function to get free time slots, taking time slots busy into account

--- app/orders/utils.py
+++ app/orders/utils.py
@@ -1,7 +1,8 @@
+from collections import Counter
 from datetime import time
 
 from fastapi import Query
-from app.orders.config_orders import START_WALK_TIME, END_WALK_TIME
+from app.orders.config_orders import START_WALK_TIME, END_WALK_TIME, COUNT_WALKER
 from app.orders.exceptions import UncorrectedTimeOrderHTTPException, TimeIntervalOrderHTTPException
 
 
@@ -16,3 +17,10 @@ def validate_walk_time(walk_time: time = Query(...,
     if not (START_WALK_TIME <= walk_time <= END_WALK_TIME):
         raise TimeIntervalOrderHTTPException
     return walk_time
+
+
+def get_available_times_in_selected_day(possible_walking_time: list, busy_times: list):
+    busy_count = Counter(busy_times)  # подсчет повторений
+    # время считается доступным, если один из выгуливателей питомцев (COUNT_WALKER) свободен в это время
+    available_times = [t for t in possible_walking_time if busy_count[t] < COUNT_WALKER]
+    return available_times
