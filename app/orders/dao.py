09df00fd6f7e7bdcbb25326512cdaaaff5408e78
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-08-07 18:57:51 +0300

    created method to get busy walking time on the selected day

--- app/orders/dao.py
+++ app/orders/dao.py
@@ -1,9 +1,32 @@
+import pydantic
+from datetime import date, time
 from sqlalchemy import and_, func, insert, or_, select
 from sqlalchemy.exc import SQLAlchemyError
 
+from app.dao.exception import DaoMethodException
+from app.db.base_model import async_session_maker
 from app.orders.model import Orders
 from app.dao.base_dao import BaseDAO
 
 
 class OrdersDAO(BaseDAO):
     model = Orders
+
+    @classmethod
+    @pydantic.validate_call
+    async def get_busy_time_in_selected_date(cls, selected_date: date) -> list[time]:
+        try:
+            async with async_session_maker() as session:
+                query = select(cls.model.walk_time).filter_by(walk_date=selected_date)
+                result_query = await session.execute(query)
+                busy_times = result_query.scalars().all()
+                print(f"{busy_times=}")
+                return busy_times
+        except (SQLAlchemyError, Exception) as error:
+            msg = "Database Exc" if isinstance(error, SQLAlchemyError) else "Unknown Exc"
+            msg += ": Cannot get meme with pagination"
+            extra = {
+                "selected_date": selected_date,
+            }
+            # logger.error(msg, extra=extra, exc_info=True)
+            raise DaoMethodException(error)
