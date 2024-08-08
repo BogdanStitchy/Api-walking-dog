627c2319ac20cb1b88ff356c5764df97793fff22
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-08-08 11:06:48 +0300

    not null restrictions added

--- app/orders/model.py
+++ app/orders/model.py
@@ -7,11 +7,11 @@ class Orders(Base):
     __tablename__ = 'orders'
 
     id = Column(Integer, primary_key=True, index=True)
-    apartment_number = Column(String)
-    pet_name = Column(String)
-    pet_breed = Column(String)
-    walk_date = Column(Date, index=True)
-    walk_time = Column(Time)
+    apartment_number = Column(String, nullable=False)
+    pet_name = Column(String, nullable=False)
+    pet_breed = Column(String, nullable=False)
+    walk_date = Column(Date, index=True, nullable=False)
+    walk_time = Column(Time, nullable=False)
 
     def __str__(self):
         return f"Order #{self.id}\tpet: {self.pet_name} ({self.pet_breed})"
